b8f4dc8ccdfcbb125e2b202aae034ed628f1c4be
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-05 20:22:52 +0000

    first commit

---
+++ lib/transfermarkt.rb
@@ -0,0 +1,6 @@
+require "transfermarkt/version"
+
+module Transfermarkt
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/transfermarkt/version.rb
@@ -0,0 +1,3 @@
+module Transfermarkt
+  VERSION = "0.1.0"
+end
