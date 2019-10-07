907764f90872b2552bbe6b49d421ca9ee4116e05
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-07 20:09:11 +0000

    added next url

--- lib/transfermarkt/scraper.rb
+++ lib/transfermarkt/scraper.rb
@@ -3,42 +3,6 @@ class Transfermarkt::Scraper
  
  attr_accessor :doc, :url
 
-  # def scrape_players_index
-  #   self.doc.css("div.box tbody").first.css("tbody tr.odd")
-  # end 
-  
-  # def create_players
-  #   scrape_players_index.each do |player|
-  #     Transfermarkt::Player.new_from_index_page(player)
-  #   end 
-  # end 
-  
-  # def self.players #change name  
-    
-  #   self.scrape_players
-  # end 
-  
-  
-  # def self.scrape_results
-  #   # Transfermarkt::PlayerScraper.new("https://www.transfermarkt.us/schnellsuche/ergebnis/schnellsuche?query=#{playername}&x=0&y=0")
-    
-  #   players = []
-  #   managers_staff = []
-  #   teams = []
-    
-  #   # @players << self.scrape_players 
-  #   managers_staff << self.scrape_managers_staff 
-  #   teams << self.scrape_teams
-    
-  #       # go to transfermarkt search results based on input from user 
-  #       # extract search results for players - later staff and teams 
-  #       # instatiate a player/team/manager
-
-  #   players
-  #   managers_staff
-  #   teams
-  # end
-  
   
   def self.set_doc(query)
     @url = "https://www.transfermarkt.us/schnellsuche/ergebnis/schnellsuche?query=#{query}&x=0&y=0"
@@ -46,8 +10,7 @@ class Transfermarkt::Scraper
   end 
   
   def self.next_url
-    btns = @doc
-    
+    next_btn = @doc.css("div.row div.box div.responsive-table div.pager li.naechste-seite a").attribute("href").value
   end 
 
     
@@ -55,7 +18,6 @@ class Transfermarkt::Scraper
   def self.scrape_players(url)
     players = []
     player_array = @doc.css("div.box").first #first box with player results
-    
     player_array.css("table.items tbody tr.odd").each_with_index do |player, i| #add the evens or find a way to add both
     
         players[i] = {
