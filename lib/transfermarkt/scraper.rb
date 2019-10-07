0ddc16c287f077356da087d0db35f505b3456577
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-07 22:50:24 +0000

    both odd and even players work

--- lib/transfermarkt/scraper.rb
+++ lib/transfermarkt/scraper.rb
@@ -1,7 +1,8 @@
 
+
 class Transfermarkt::Scraper 
  
- attr_accessor :doc, :url
+ attr_accessor :doc, :url, :new_url
 
   
   def self.set_doc(query)
@@ -10,11 +11,12 @@ class Transfermarkt::Scraper
   end 
   
   def self.next_url
-    if next_btn = @doc.css("div.row div.box div.responsive-table div.pager li.naechste-seite a")
+    next_btn = @doc.css("div.row div.box div.responsive-table div.pager li.naechste-seite a")
+      if next_btn.attribute("href")
        "https://www.transfermarkt.com" + next_btn.attribute("href").value
-    else 
-      nil
-    end 
+      else 
+        nil
+      end 
   end 
   
 
@@ -24,9 +26,10 @@ class Transfermarkt::Scraper
     players = []
     # loop do
       player_array = @doc.css("div.box").first #first box with player results
-      player_array.css("table.items tbody tr.odd").each_with_index do |player, i| #add the evens or find a way to add both
+      player_array.css("table.items > tbody > tr").each do |player| #add the evens or find a way to add both
       
-          players[i] = {
+      
+          players << {
             :name => player.css("td.hauptlink a").text, 
             :position => player.css("td.zentriert").first.text,
             :club => player.css("a.vereinprofil_tooltip").text,
@@ -35,15 +38,18 @@ class Transfermarkt::Scraper
             :market_value => player.css("td.rechts").first.text,
             :agents => player.css("td.rechts a").text
           }
+          
       @new_url = self.next_url
+      
         # if @new_url
         #   @doc = Nokogiri::HTML(open(@new_url))
         # else 
         #   break
         # end 
       end 
-      players
+      
     # end 
+    players
   end 
   
   def self.scrape_managers_staff
