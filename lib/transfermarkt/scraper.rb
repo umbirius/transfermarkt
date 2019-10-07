bdc7fe6fcc66d0f1b188ee400138081b91b27611
Umberto Palazzo <upalazzo93@gmail.com>
2019-10-07 20:42:46 +0000

    added loop for new url, but does not work

--- lib/transfermarkt/scraper.rb
+++ lib/transfermarkt/scraper.rb
@@ -10,28 +10,42 @@ class Transfermarkt::Scraper
   end 
   
   def self.next_url
-    next_btn = @doc.css("div.row div.box div.responsive-table div.pager li.naechste-seite a").attribute("href").value
+    if next_btn = @doc.css("div.row div.box div.responsive-table div.pager li.naechste-seite a")
+       "https://www.transfermarkt.com" + next_btn.attribute("href").value
+    else 
+      nil
+    end 
   end 
+  
 
     
   
   def self.scrape_players(url)
     players = []
-    player_array = @doc.css("div.box").first #first box with player results
-    player_array.css("table.items tbody tr.odd").each_with_index do |player, i| #add the evens or find a way to add both
-    
-        players[i] = {
-          :name => player.css("td.hauptlink a").text, 
-          :position => player.css("td.zentriert").first.text,
-          :club => player.css("a.vereinprofil_tooltip").text,
-          :age => player.css("td.zentriert")[2].text,
-          :nationality => player.css("td.zentriert img")[1].attribute("title").value,
-          :market_value => player.css("td.rechts").first.text,
-          :agents => player.css("td.rechts a").text
-        }
-    
+    binding.pry
+    loop do
+      player_array = @doc.css("div.box").first #first box with player results
+      player_array.css("table.items tbody tr.odd").each_with_index do |player, i| #add the evens or find a way to add both
+      
+          players[i] = {
+            :name => player.css("td.hauptlink a").text, 
+            :position => player.css("td.zentriert").first.text,
+            :club => player.css("a.vereinprofil_tooltip").text,
+            :age => player.css("td.zentriert")[2].text,
+            :nationality => player.css("td.zentriert img")[1].attribute("title").value,
+            :market_value => player.css("td.rechts").first.text,
+            :agents => player.css("td.rechts a").text
+          }
+      @new_url = self.next_url
+      binding.pry
+        if new_url
+          @doc = Nokogiri::HTML(open(@new_url))
+        else 
+          break
+        end 
+      end 
+      players
     end 
-    players
   end 
   
   def self.scrape_managers_staff
